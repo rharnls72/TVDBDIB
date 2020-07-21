d9e943a1115f0ebb79bd5eb27862109fe6c6211f
Minseong Seo <cbnm943@gmail.com>
2020-07-21 22:50:17 +0900

    [Feat] 알림 백엔드 구현 생성: Alert.java, AlertDao.java, AlertDao.xml, AlertController.java 수정: application.properties

---
+++ backend/src/main/java/com/web/curation/dao/alert/AlertDao.java
@@ -0,0 +1,26 @@
+package com.web.curation.dao.alert;
+
+import java.util.List;
+
+import com.web.curation.model.alert.Alert;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface AlertDao {
+    // Create
+    int addNewAlert(Alert alert);
+
+    // Read
+    List<Alert> getAlertList(int uno);
+
+    // Update
+    int setReadAllAlert(int uno);
+    int setReadAlert(int ano);
+
+    // Delete
+    int deleteAllAlert(int uno);
+    int deleteAlert(int ano);
+}
