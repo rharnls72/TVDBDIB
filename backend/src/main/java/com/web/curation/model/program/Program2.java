1fd9ebf2f9ad35a9369a20d37f7054d75250bd5a
Minseong Seo <cbnm943@gmail.com>
2020-07-27 10:16:07 +0900

    [Refector] 백엔드 프로젝트 내 warning 원인 제거 수정: EpisodeController.java, Episode2.java, Program2.java

--- backend/src/main/java/com/web/curation/model/program/Program2.java
+++ backend/src/main/java/com/web/curation/model/program/Program2.java
@@ -5,9 +5,7 @@ import java.util.ArrayList;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
 @NoArgsConstructor
