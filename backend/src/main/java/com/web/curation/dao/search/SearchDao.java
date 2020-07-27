b11abfe09ef90f3d27fe62c2a5793d05a76849e2
fnladl72 <fnladl72@naver.com>
2020-07-27 17:44:29 +0900

    [Feat] 유저 검색 기능 추가 생성 : SearchController.java, SearchDao.java, SearchDao.xml

---
+++ backend/src/main/java/com/web/curation/dao/search/SearchDao.java
@@ -0,0 +1,16 @@
+package com.web.curation.dao.search;
+
+import java.util.List;
+
+import com.web.curation.model.feed.Feed;
+import com.web.curation.model.user.User;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface SearchDao {
+   List<User> searchByNickName(String str);
+   List<Feed> searchByFeedTag(String str);
+}
