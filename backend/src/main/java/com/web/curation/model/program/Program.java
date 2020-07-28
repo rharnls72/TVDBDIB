5a194c4a9f29c0eb5d2a8416462d857c60fad0b0
Minseong Seo <cbnm943@gmail.com>
2020-07-28 17:04:30 +0900

    [Fix] 프로그램 정보 조회 시 좋아요 수, 좋아요 눌렀는지 여부 함께 반환하도록 수정 수정: ProgramController.java, LikeDao.java, LikeDao.xml, Like.java, Program.java, application.properties 삭제: ProgramDao.java, ProgramDao.xml

--- backend/src/main/java/com/web/curation/model/program/Program.java
+++ backend/src/main/java/com/web/curation/model/program/Program.java
@@ -1,6 +1,6 @@
 package com.web.curation.model.program;
 
-import java.time.LocalDateTime;
+import com.web.curation.model.reply.Reply;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,97 +10,59 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Program {
+    // 프로그램 고유번호는 꼭 필요함
     private int pno;
-    private String name;
-    private String broadcaster;
-    private String genre;
-    private int season;
-    private String alias;
-    private LocalDateTime start_date;
-    private LocalDateTime end_date;
-    private String director;
-    private String cast;
-    private String description;
-    private String broadcast_time;
-    private String thumbnail;
+
+    /////////////////////////////////////////////
+    // API 로 받아온 프로그램 정보와 매핑될 변수들 만드는 영역
+
+    /////////////////////////////////////////////
+
+    // 프로그램 정보와 함께 넘길 정보들(좋아요 수, 좋아요 여부, 댓글 수, 대표 댓글 한 개)
+    private int like_num;
+    private int reply_num;
+    private boolean press_like;
+    private Reply reply;
+
+    public int getPno() {
+        return this.pno;
+    }
 
     public void setPno(int pno) {
         this.pno = pno;
     }
-    public int getPno() {
-        return pno;
+
+    public int getLike_num() {
+        return this.like_num;
     }
-    public void setName(String name) {
-        this.name = name;
+
+    public void setLike_num(int like_num) {
+        this.like_num = like_num;
     }
-    public String getName() {
-        return name;
+
+    public int getReply_num() {
+        return this.reply_num;
     }
-    public void setBroadcaster(String broadcaster) {
-        this.broadcaster = broadcaster;
+
+    public void setReply_num(int reply_num) {
+        this.reply_num = reply_num;
     }
-    public String getBroadcaster() {
-        return broadcaster;
+
+    public boolean isPress_like() {
+        return this.press_like;
     }
-    public void setGenre(String genre) {
-        this.genre = genre;
+
+    public void setPress_like(boolean press_like) {
+        this.press_like = press_like;
     }
-    public String getGenre() {
-        return genre;
+
+    public Reply getReply() {
+        return this.reply;
     }
-    public void setSeason(int season) {
-        this.season = season;
+
+    public void setReply(Reply reply) {
+        this.reply = reply;
     }
-    public int getSeason() {
-        return season;
-    }
-    public void setAlias(String alias) {
-        this.alias = alias;
-    }
-    public String getAlias() {
-        return alias;
-    }
-    public void setStart_date(LocalDateTime start_date) {
-        this.start_date = start_date;
-    }
-    public LocalDateTime getStart_date() {
-        return start_date;
-    }
-    public void setEnd_date(LocalDateTime end_date) {
-        this.end_date = end_date;
-    }
-    public LocalDateTime getEnd_date() {
-        return end_date;
-    }
-    public void setDirector(String director) {
-        this.director = director;
-    }
-    public String getDirector() {
-        return director;
-    }
-    public void setCast(String cast) {
-        this.cast = cast;
-    }
-    public String getCast() {
-        return cast;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setBroadcast_time(String broadcast_time) {
-        this.broadcast_time = broadcast_time;
-    }
-    public String getBroadcast_time() {
-        return broadcast_time;
-    }
-    public void setThumbnail(String thumbnail) {
-        this.thumbnail = thumbnail;
-    }
-    public String getThumbnail() {
-        return thumbnail;
-    }
+
 }
 
