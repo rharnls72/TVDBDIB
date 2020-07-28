413b5f1cb8b49e33838a36753394d594a39ad3ca
박하은 <eun-8279@hanmail.net>
2020-07-28 13:33:05 +0900

    [Feat] 내 프로필 페이지 Header 생성 생성 : MyPage.vue, MyPageHeader.vue 수정 : routes.js

---
+++ frontend/src/views/account/mine/MyPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <MyPageHeader :nickname="nickname"/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import MyPageHeader from '../../../components/account/mine/MyPageHeader.vue'
+
+export default {
+  name: 'MyPage',
+  data() {
+    return {
+      nickname: 'hani',
+    }
+  },
+  components: {
+    MyPageHeader,
+  },
+  methods: {
+    // getNickname() {
+    //   axios.get('http://localhost:9000/')
+    //     .then(res => {
+
+    //     })
+    //     .catch(err => console.error(err))
+    // },
+  },
+}
+</script>
+
+<style>
+
+</style>
