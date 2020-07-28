9d8c524a5ac630f6b25470a2e69a2303baa1a9fb
박하은 <eun-8279@hanmail.net>
2020-07-28 16:18:32 +0900

    [Fix] 내 프로필 페이지 정보 틀 완성 수정 : MyPage.vue, MyPageInformation.vue

--- frontend/src/components/account/mine/MyPageInformation.vue
+++ frontend/src/components/account/mine/MyPageInformation.vue
@@ -3,10 +3,10 @@
     <div class="wrapB">
       <div class="container mt-3">
         <div class="row p-0">
-          <img class="col-3 p-0" :src="defaultProfile" alt="default-image">
+          <img class="col-3 p-0 mb-3" :src="defaultProfile" alt="default-image">
           <div class="col-3 my-3">
             <h5 class="mb-0 text-center">340</h5>
-            <p class="mb-0 text-center">좋아요</p>
+            <p class="mb-0 text-center">게시물</p>
           </div>
           <div class="col-3 my-3">
             <h5 class="mb-0 text-center">340</h5>
@@ -17,6 +17,10 @@
             <p class="mb-0 text-center">팔로잉</p>
           </div>
         </div>
+        <p class="row mb-0 pb-3 introduce">나 이런 사람이야~ 나 이런 사람이야~ 나 이런 사람이야~ 나 이런 사람이야~ 나 이런 사람이야~</p>
+        <p class="row p-0">
+          <button class="col-12 mybutton p-0 text-dark">프로필 수정</button>
+        </p>
       </div>
     </div>
   </div>
@@ -36,5 +40,13 @@ export default {
 </script>
 
 <style>
-
+  .introduce {
+    line-height: 1.5em;
+  }
+  .mybutton {
+    height: auto;
+    box-shadow: none;
+    border: 1px solid lightgray;
+    border-radius: 0.25rem;
+  }
 </style>
