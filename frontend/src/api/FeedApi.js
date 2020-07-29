09ac07db95baf0dfe84466846d2abec3571c5e40
Minseong Seo <cbnm943@gmail.com>
2020-07-29 13:32:29 +0900

    [Fix] 피드 목록 조회시 가장 좋아요 많은 댓글 내용도 같이 주도록 수정 + 프론트 비동기 요청 부분 수정 수정: FeedDao.xml, Feed.java, FeedApi.js, http-common.js, IndexFeed.vue

--- frontend/src/api/FeedApi.js
+++ frontend/src/api/FeedApi.js
@@ -7,13 +7,7 @@ const createFeed = (data,callback,errorCallback) => {
     // 성공 : call back 호출
     // 실패 :  errorCallback 호출
 
-    //백앤드와 로그인 통신하는 부분
-
-    http.post('/feed/create', data, {headers: {
-        "Content-type": "application/json",
-        "jwt-token": sessionStorage.getItem('jwt-token'),
-        "Access-Control-Allow-Origin": "*"
-    }})
+    http.post('/feed/create', data, header())
         .then(res => {
             // 서버에서 정상적으로 처리되었으면 res 가 null 이 될 수 없음
             // 서버에서 db 쿼리 하다 오류난 경우일듯
@@ -40,8 +34,41 @@ const createFeed = (data,callback,errorCallback) => {
         });
 }
 
+const getFeedList = (data,callback,errorCallback) => {
+    // 피드 조회
+    // 성공 : call back 호출
+    // 실패 :  errorCallback 호출
+
+    http.post('/feed/list', data, header())
+        .then(res => {
+            // 서버에서 정상적으로 처리되었으면 res 가 null 이 될 수 없음
+            // 서버에서 db 쿼리 하다 오류난 경우일듯
+            if(res == null) {
+                let error = {msg : '알 수 없는 오류 발생'};
+                errorCallback(error);
+            }
+            // 서버에서 처리되어 데이터가 제대로 넘어왔을 때
+            else {
+                // 피드 조회 성공
+                if(res.data.status) {
+                    callback(res);
+                }
+                // 피드 조회 실패
+                else {
+                    let error = {msg : res.data.msg};
+                    errorCallback(error);
+                }
+            }
+        })
+        .catch(err => {
+            err.msg = '서버 요청에서 오류 발생';
+            errorCallback(err);
+        });
+}
+
 const FeedApi = {
     createFeed:(data,callback,errorCallback)=>createFeed(data,callback,errorCallback)
+    , getFeedList:(data,callback, errorCallback)=>getFeedList(data, callback, errorCallback)
 }
 
 export default FeedApi
