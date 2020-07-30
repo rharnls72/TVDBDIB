6be42c46dad26fb9f77601d60101e909c3a383fe
fnladl72 <fnladl72@naver.com>
2020-07-30 16:41:50 +0900

    [Feat] 프로필 수정 백/프론트 연결 수정 : AccountController.java, UserDao.xml, AccountApi.js, EditMyPageForm.vue, EditMyPageHeader.vue, MyPageInformation.vue, EditMyPge.vue

--- frontend/src/components/account/mine/MyPageInformation.vue
+++ frontend/src/components/account/mine/MyPageInformation.vue
@@ -19,7 +19,7 @@
         </div>
         <p class="row mb-0 pb-3 introduce">{{info.bio}}</p>
         <p class="row p-0">
-          <button class="col-12 mybutton p-0 text-dark">프로필 수정</button>
+          <button class="col-12 mybutton p-0 text-dark" @click="editmypage">프로필 수정</button>
         </p>
       </div>
     </div>
@@ -38,6 +38,11 @@ export default {
   data() {
     return {
       defaultProfile,
+    }
+  },
+  methods: {
+    editmypage(){
+      this.$router.push({name:'EditMyPage'})
     }
   },
 }
