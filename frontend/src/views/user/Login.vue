77d87e36b17d1c7ebf677c3f2e085073e2ae55c5
Minseong Seo <cbnm943@gmail.com>
2020-07-30 11:37:43 +0900

    [Fix] 테스트해보다 로그인이 안되서 수정 수정: Login.vue

--- frontend/src/views/user/Login.vue
+++ frontend/src/views/user/Login.vue
@@ -92,8 +92,9 @@ export default {
   created() {
     GetUserApi.getUser(res => {
       this.$store.commit('addUserInfo', res.user);
+      // 비동기 요청이 완료되었을 때 store 에 유저 정보가 있는지 확인해야함(여기에 위치해야함)
+      if(this.$store.state.userInfo != null)  this.$router.push({name:'IndexCuration'});
     });
-    if(this.$store.state.userInfo != null)  this.$router.push({name:'IndexCuration'});
 
     this.component = this;
 
