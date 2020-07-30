54196ce1c0a64c116dec0f0a8ff83b070ff342ea
박하은 <eun-8279@hanmail.net>
2020-07-30 16:39:07 +0900

    [Fix] 댓글 컴포넌트 수정 수정 : ReplyItem.vue, ReReplyItem.vue

--- frontend/src/components/ReplyItem.vue
+++ frontend/src/components/ReplyItem.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="feed-item" style="border-bottom: none;">
+  <div class="feed-item pb-0 mb-0" style="border-bottom: none;">
     <div class="d-flex justify-align-between align-items-center mt-2">
       <b-form-input type="text" class="m-0 rounded-pill" v-model="content" placeholder="내용 입력!!!"></b-form-input>
-      <b-icon icon="plus-circle" class="text-right ml-2" font-scale="1.4"></b-icon>
+      <!-- 댓글 내용이 없으면 -->
+      <b-icon v-if="!content" icon="plus-circle" class="text-right ml-2 text-light" font-scale="1.4"></b-icon>
+      <!-- 댓글 내용이 있으면 -->
+      <b-icon v-else icon="plus-circle" class="text-right ml-2 text-secondary" font-scale="1.4"></b-icon>
     </div>
     <div class="mt-2 pl-2 pr-2">
       <div v-for="r in replies" :key="r.id">
@@ -25,10 +28,10 @@ export default {
       replies: [
         {id:1, user: 'chsmd', contents:'소통'}, 
         {id:2, user: 'chsmd', contents:'소통'}, 
-        {id:3, user: 'chsmd', contents:'소통'}, 
-        {id:4, user: 'chsmd', contents:'소통'}, 
-        {id:5, user: 'chsmd', contents:'소통'}, 
-        {id:6, user: 'chsmd', contents:'소통'}, 
+        // {id:3, user: 'chsmd', contents:'소통'}, 
+        // {id:4, user: 'chsmd', contents:'소통'}, 
+        // {id:5, user: 'chsmd', contents:'소통'}, 
+        // {id:6, user: 'chsmd', contents:'소통'}, 
       ],
       reReplies: [
         {id:1, user: 'chsmd', contents:'소통'}, 
