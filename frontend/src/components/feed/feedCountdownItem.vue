2fc10db46a7390554f8a1b9222cbc79f6c6de0b2
손초능 <thschsmd@naver.com>
2020-07-31 00:00:18 +0900

    [Fix] Frontend 기능 수정 및 보완 수정 : feedArticleItem.vue, feedCountdownItem.vue, feedVoteItem.vue, IndexFeed.vue

--- frontend/src/components/feed/feedCountdownItem.vue
+++ frontend/src/components/feed/feedCountdownItem.vue
@@ -4,19 +4,15 @@
       <div class="profile-image" :style="{'background-image': 'url('+defaultProfile+')'}"></div>
       <div class="user-info">
         <div class="user-name">
-          <button>SSAFY</button>
+          <button>{{nick_name}}</button>
         </div>
         <p class="date">{{createAfter}} 시간 전</p>
       </div>
       <div class="content d-flex flex-comlumn justify-content-between align-items-center my-2">
         <div>{{feedTitle}}</div>
-        <b-icon v-b-toggle.sidebar-1 icon="three-dots-vertical" font-scale="1.3"></b-icon>
-        <b-sidebar id="sidebar-1" shadow>
-          <div class="ml-3">
-            <div @click="updateFeed">수정</div>
-            <div @click="delFeed">삭제</div>
-          </div>
-        </b-sidebar>
+        <div v-if="!!this.$store.state.userInfo && this.$store.state.userInfo.uno === writer_uno">
+          <span @click="updateFeed">수정</span>   <span @click="delFeed">삭제</span>
+        </div>
       </div>
     </div>
     <div class="feed-card">
@@ -50,12 +46,14 @@
             <b-icon-heart v-if="!likeIcon"></b-icon-heart>
             <b-icon-heart-fill v-else variant="danger"></b-icon-heart-fill>
           </button>
+          {{like_num}}
         </div>
         <!-- 댓글 -->
         <div class="mr-3">
           <button class="h6 mr-1">
             <b-icon-chat></b-icon-chat>
           </button>
+          {{reply_num}}
         </div>
         <!-- 스크랩 -->
         <div class="mr-3">
@@ -84,6 +82,7 @@
       <span v-for="tag in tags" :key="tag" class="tag">#{{tag}} </span><br>
       <span v-if="!isStretch" class="moreView" @click="turnStretch">댓글 {{reply_num}}개</span>
     </div>
+    <span class="font-weight-bold">{{reply_user_nick}} </span>{{reply_content}}
     <ReplyItem v-if="isStretch" :fno="fno"/>
   </div>
     <!---->
@@ -118,6 +117,10 @@ export default {
         likeIcon: false,
         scrapIcon: false,
         isStretch: false,
+        create_date: null,
+        reply_content: null,
+        reply_user_nick: [],
+        nick_name: null,
       }
     },
     props: {
@@ -220,6 +223,9 @@ export default {
       this.writer_uno = this.article.uno
       this.likeIcon = this.article.press_like
       this.scrapIcon = this.article.press_dibs
+      this.reply_content = this.article.reply_content
+      this.reply_user_nick = this.article.reply_user_nick
+      this.nick_name = this.article.nick_name
       if (!this.article.dibsNum) {this.scrapNum = 0}
       else {this.scrapNum = this.article.dibsNum}
       this.create_date = this.article.create_date
