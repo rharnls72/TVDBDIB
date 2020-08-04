a7c2b7ba8f6aa904bb1b1350e2c007452468e1e5
박하은 <eun-8279@hanmail.net>
2020-08-04 17:30:36 +0900

    [Feat] 내 프로필 페이지 태그된 피드 틀 구현 수정 : MyPageTab.vue, TaggedFeed.vue

--- frontend/src/components/account/mine/TaggedFeed.vue
+++ frontend/src/components/account/mine/TaggedFeed.vue
@@ -1,18 +1,40 @@
 <template>
-  <div>
-    <h1>태그된 글</h1>
+  <div class="col-12 m-0 p-0">
+    <div v-for="feed in feeds" :key="feed.fno" class="col-12 row m-0 px-0 py-2 align-items-center">
+      <!-- {{ feed }} -->
+      <div class="col-6 mythumbnail">
+        <!-- <FeedImage /> -->
+        <h1 class="text-center m-0">썸네일</h1>
+        <h1 class="text-center m-0">이미지</h1>
+      </div>
+      <div class="col-6">
+        <p class="m-0">{{ feed.content.title }}</p>
+        <p class="m-0">{{ feed.content.content }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
+// import FeedImage from '../../../components/feed/FeedImage.vue'
+
 export default {
   name: 'TaggedFeed',
   props: {
     feeds: Array,
-  }
+  },
+  // components: {
+  //   FeedImage,
+  // },
 }
 </script>
 
-<style>
-
+<style scoped>
+  .mythumbnail {
+    border: 1px solid lightgray;
+    width: 100%;
+    height: auto;
+  }
 </style>
