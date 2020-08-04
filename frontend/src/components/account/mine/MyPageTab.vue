a378f1c8194ef554d970fd0a134db4b59b37c34f
박하은 <eun-8279@hanmail.net>
2020-08-04 12:49:03 +0900

    [Feat] 내 프로필 페이지 피드 탭 생성 생성 : MyPageTab.vue 수정 : MyPage.vue, MyPageInformation.vue

---
+++ frontend/src/components/account/mine/MyPageTab.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="p-0">
+    <b-tabs class="mytabs" active-nav-item-class="font-weight-bold text-dark" content-class="mt-3" justified>
+      <b-tab title="내가 쓴" title-link-class="text-secondary" active></b-tab>
+      <b-tab title="태그된" title-link-class="text-secondary"></b-tab>
+    </b-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyPageTab',
+}
+</script>
+
+<style>
+
+</style>
