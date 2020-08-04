46fa37f9ecfe91d631b9e2ae9e1cbb3ca48dda43
박하은 <eun-8279@hanmail.net>
2020-08-04 17:25:39 +0900

    [Style] 내 프로필 페이지 내가 쓴 피드 주석 정리 수정 : WrittenFeed.vue

--- frontend/src/components/account/mine/WrittenFeed.vue
+++ frontend/src/components/account/mine/WrittenFeed.vue
@@ -2,9 +2,6 @@
   <div class="col-12 m-0 p-0">
     <div v-for="feed in feeds" :key="feed.fno" class="col-12 row m-0 px-0 py-2 align-items-center">
       <!-- {{ feed }} -->
-      <!-- <feedArticleItem v-if="feed.ctype===1" :article="feed" :fno="feed.fno"/>
-      <feedCountdownItem v-if="feed.ctype===2" :article="feed" :fno="feed.fno"/>
-      <feedVoteItem v-if="feed.ctype===3" :article="feed" :fno="feed.fno"/> -->
       <div class="col-6 mythumbnail">
         <!-- <FeedImage /> -->
         <h1 class="text-center m-0">썸네일</h1>
@@ -23,22 +20,13 @@ import { mapState } from "vuex";
 
 // import FeedImage from '../../../components/feed/FeedImage.vue'
 
-// import feedArticleItem from '@/components/feed/feedArticleItem.vue'
-// import feedCountdownItem from '@/components/feed/feedCountdownItem.vue'
-// import feedVoteItem from '@/components/feed/feedVoteItem.vue'
-
 export default {
   name: 'WrittenFeed',
-  // components: {
-  //   FeedImage,
-  // },
   props: {
     feeds: Array,
   },
-  // components: { 
-  //   feedArticleItem, 
-  //   feedCountdownItem, 
-  //   feedVoteItem,
+  // components: {
+  //   FeedImage,
   // },
 }
 </script>
