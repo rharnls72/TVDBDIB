67e7335fa17096e2d9699c93305fba534e68426d
박하은 <eun-8279@hanmail.net>
2020-08-05 13:38:37 +0900

    [Feat] 스크랩한 게시물 컴포넌트 생성 및 스크랩 페이지에 추가 생성 : Scrapped.vue 수정 : IndexScrap.vue

---
+++ frontend/src/components/account/mine/Scrapped.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="col-12 m-0 p-0">
+    <h1>찜한 게시물</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Scrapped',
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/account/mine/IndexScrap.vue
+++ frontend/src/views/account/mine/IndexScrap.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <IndexScrapHeader />
+    <div class="container mycontainer">
+      <Scrapped />
+    </div>
     <Footer />
   </div>
 </template>
@@ -8,16 +11,21 @@
 <script>
 import IndexScrapHeader from '../../../components/account/mine/IndexScrapHeader.vue'
 import Footer from '../../../components/common/custom/Footer.vue'
+import Scrapped from '../../../components/account/mine/Scrapped.vue'
 
 export default {
   name: 'IndexScrap',
   components: {
     IndexScrapHeader,
     Footer,
+    Scrapped,
   },
 }
 </script>
 
-<style>
-
+<style scoped>
+  .mycontainer {
+    padding-top: 70px;
+    padding-bottom: 50px;
+  }
 </style>
