32dd66a16ee15d92c7032fa2426990bbdfbcb506
박하은 <eun-8279@hanmail.net>
2020-08-05 13:24:49 +0900

    [Feat] 스크랩 페이지에 Header와 Footer 추가 수정 : IndexScrap.vue

--- frontend/src/views/account/mine/IndexScrap.vue
+++ frontend/src/views/account/mine/IndexScrap.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-
+    <IndexScrapHeader />
+    <Footer />
   </div>
 </template>
 
 <script>
+import IndexScrapHeader from '../../../components/account/mine/IndexScrapHeader.vue'
+import Footer from '../../../components/common/custom/Footer.vue'
+
 export default {
   name: 'IndexScrap',
+  components: {
+    IndexScrapHeader,
+    Footer,
+  },
 }
 </script>
 
