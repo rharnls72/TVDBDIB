94fc4341c0cb6cf881e59ca0e2597c50784cd5ab
손초능 <thschsmd@naver.com>
2020-08-06 10:06:45 +0900

    [Feat] 피드 파일 생성하기 수정 : CreateArticle.vue 생성 : FeedArticleMiniTab.vue

--- frontend/src/components/feed/CreateArticle.vue
+++ frontend/src/components/feed/CreateArticle.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="user join wrapC">
+  <div class="user join wrapC" id="article">
     <div class="d-flex justify-content-between align-items-center mb-3">
       <b-icon @click="moveMain" icon="chevron-left" font-scale="1.5"></b-icon>
       <b-icon @click="submitArticle" icon="check-square" font-scale="1.4"></b-icon>
