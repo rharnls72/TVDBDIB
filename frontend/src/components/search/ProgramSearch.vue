a5200ec8c60661e96f5d5f7fea11fbea8e9ac05a
윤수연 <pureyeon221@gmail.com>
2020-08-08 15:10:40 +0900

    [Feat] 출연자명으로 검색 기능 1차 구현 (보완 필요) 수정: ProgramSearch.vue

--- frontend/src/components/search/ProgramSearch.vue
+++ frontend/src/components/search/ProgramSearch.vue
@@ -3,31 +3,35 @@
     <b-nav justified class="myheader" >
       <b-input-group class="align-items-center m-2 mysearchbar">
         <div class="input-group-prepend">
-          <div @click="searchIcon()" class="input-group-text py-0" style="border: 0px; background-color: #eee;">
+          <div @click="searchIcon()" class="input-group-text py-0" style="margin-left: -10%; border: 0px; background-color: #eee;">
             <b-icon-search></b-icon-search>
           </div>
         </div>
 
+        <!-- 프로그램명 / 인명 검색 select -->
+        <b-form-select v-model="selected" :options="options"
+         style="height: 85%; width: 20%; border: 0px;"></b-form-select>
+
         <!-- 자동완성 기능 지원하는 검색 창 컴포넌트 -->
         <vue-bootstrap-typeahead
-          :data="programs"
+          :data="searchedList"
           v-model="word"
           size="sm"
           textVariant="red"
           :serializer="p => p.name"
           :minMatchingChars='1'
           :maxMatches='20'
-          placeholder="type a program name"
+          placeholder="search..."
           @hit="selectedUser = $event"
           ref="searchInput"
-          style="height: auto; width: 80%; border: 0px; background-color: #eee;"
+          style="height: auto; width: 55%; border: 0px; background-color: #eee;"
         />
       </b-input-group>
     </b-nav>
 
-    <ResultItems :programs="programs_result"/>
+    <ResultItems :programs="result"/>
     <!-- v-if를 넣어서 처음 페이지 띄웠을 때 (검색버튼 안 눌렀을 때) 는 동작 안하게 함 -->
-    <infinite-loading v-if="loading_complete" @infinite="infiniteHandler"></infinite-loading>
+    <infinite-loading v-if="loading_complete && !isEndPoint" @infinite="infiniteHandler"></infinite-loading>
 
 </div>
 
@@ -45,12 +49,19 @@ export default {
 
   data() {
     return {
+      selected: 'program',
+      options: [
+        { value: 'program', text: "프로그램명" },
+        { value: 'people', text: "출연자"}
+      ],
+
       word: "",
-      programs: [],
-      programs_result: [],
+      searchedList: [],
+      result: [],
       total_pages: 0,
       current_page: 0,
-      loading_complete: false
+      loading_complete: false,
+      isEndPoint: false
       //search_history: [],
       //selectedUser: {},
     }
@@ -58,16 +69,25 @@ export default {
   
   watch: {
     word(newWord) {
-        this.getProgramList(newWord);
+        if (this.selected == 'program')
+          this.getProgramList(newWord);
+        else if (this.selected == 'people')
+          this.getPeopleList(newWord);
     },
   },
   
   methods: {
     infiniteHandler($state) {
       setTimeout(() => {
-        if (this.loading_complete == true && this.current_page < this.total_pages){
+
+        if (this.current_page == this.total_pages){
+          this.isEndPoint = true;
+        }
+
+        if (this.loading_complete == true && !this.isEndPoint){
           this.toNextPage();
         }
+
         $state.loaded();
       }, 1000);
     },
@@ -90,7 +110,19 @@ export default {
         axios.get(tmdbApi.BASE_URL + "search/tv?query=" + newWord + "&api_key=" + tmdbApi.API_KEY + "&language=ko")
             .then(res => {
                 console.log(res);
-                this.programs = res.data.results;
+                this.searchedList = res.data.results;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
+
+    // 한글로 입력하면 분명 검색결과는 받아오는데 원문이 영어라서 그런지 자동완성이 안된다.
+    getPeopleList(newWord){
+        axios.get(tmdbApi.BASE_URL + "search/person?query=" + newWord + "&api_key=" + tmdbApi.API_KEY + "&language=ko")
+            .then(res => {
+                console.log(res);
+                this.searchedList = res.data.results;
             })
             .catch(error => {
                 console.log(error);
@@ -98,10 +130,42 @@ export default {
     },
 
     searchIcon(){
-        axios.get(tmdbApi.BASE_URL + "search/tv?query=" + this.word + "&api_key=" + tmdbApi.API_KEY + "&language=ko")
+        if (this.selected == 'program')
+          this.getProgramResult(this.word);
+        else if (this.selected == 'people')
+          this.getProgramResultForPeople(this.word);
+    },
+
+    getProgramResult(word){
+        axios.get(tmdbApi.BASE_URL + "search/tv?query=" + word + "&api_key=" + tmdbApi.API_KEY + "&language=ko")
             .then(res => {
                 console.log(res);
-                this.programs_result = res.data.results;
+                this.result = res.data.results;
+                this.total_pages = res.data.total_pages;
+                this.current_page = 1;
+                this.loading_complete = true;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
+
+    getProgramResultForPeople(word){
+        axios.get(tmdbApi.BASE_URL + "search/person?query=" + word + "&api_key=" + tmdbApi.API_KEY + "&language=ko")
+            .then(res => {
+              // 1. 동명이인 어떻게 처리? - 다 띄워줄 수 있게 인터페이스를...?
+              // 2. known_for가 왜 3개까지밖에 안뜨지?
+                console.log(res);
+                
+                let items = res.data.results[0].known_for;
+                // 영화빼고 TV 프로그램만 넣기.
+                for (let i=0; i<items.length; i++){
+                  if (items[i].media_type == 'tv')
+                    this.result.push(items[i]);
+                }
+                
+                //this.result = res.data.results[0].known_for; 
+                
                 this.total_pages = res.data.total_pages;
                 this.current_page = 1;
                 this.loading_complete = true;
@@ -110,6 +174,9 @@ export default {
                 console.log(error);
             });
     }
+
+
+
   },
   components: {
     //SearchApi,
