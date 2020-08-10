45485cf8dd1f964b5db861ce724828400082a43d
Seo Min Seong <cbnm943@gmail.com>
2020-08-10 18:30:11 +0900

    [Fix] 패키지를 활용한 구글 로그인 구현(됐으면 좋겠다) 수정: package.json, index.html, main.js, Login.vue

--- frontend/src/views/user/Login.vue
+++ frontend/src/views/user/Login.vue
@@ -229,13 +229,13 @@ export default {
       KakaoApi.Login();
     }
     , doGoogleLogin(googleUser) {
-      var profile = googleUser.getBasicProfile();
-      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-      console.log('Name: ' + profile.getName());
-      console.log('Image URL: ' + profile.getImageUrl());
-      console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-
-      GoogleApi.login();
+      this.$gAuth.signIn()
+        .then(GoogleUser => {
+          console.log('GoogleUser', user);
+        })
+        .catch(error => {
+          this.$router.push({name:'Errors', query: {message: "Google social login fail"}});
+        });
     }
   },
   data: () => {
