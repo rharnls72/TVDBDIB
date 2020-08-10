1ac6b5c9f5f7c3116dd5d35e1ca6349522509a21
윤수연 <pureyeon221@gmail.com>
2020-08-10 17:23:08 +0900

    [Fix] 배우 이름으로 검색 기능 수정: 동명이인 처리, 모든 출연 프로그램 출력, 사진 추가 추가: PeopleSearchResult.vue 수정: ProgramSearch.vue, ProgramSearchResult.vue

---
+++ frontend/src/components/search/PeopleSearchResult.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="feed-item">
+
+        <div v-for="(people) in peoples" v-bind:key="people.id" class="card" style="width: 100%;">
+        <div class="card-header d-flex justify-content-between align-items-center">
+            {{people.people_name}}
+            <img :src='imgBaseUrl + people.profile_path' width="35rem" height="35rem" class="img-fluid float-right" alt="profile">
+        </div>
+        <ul class="list-group list-group-flush">
+            <li v-for="(program) in people.programs" v-bind:key="program.id" class="list-group-item d-flex justify-content-between align-items-center">
+                {{program.name}}
+                <div class="image-parent">
+                    <img :src='imgBaseUrl + program.poster_path' width="75rem" height="120rem" class="img-fluid" alt="poster">
+                </div>
+            </li>
+        </ul>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+import tmdbApi from '@/api/tmdbApi.js';
+
+export default {
+    name: 'ProgramSearchResult',
+  data: () => {
+    return {
+        imgBaseUrl: tmdbApi.IMAGE_BASE_URL
+    };
+  },
+  props: {
+    peoples: Array
+  },
+  methods:{
+      goDetail(id){
+          this.$router.push("/program/" + id);
+      }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  padding-top: 0px; /* 검색창을 sticky로 고정시키니까 이걸 다시 0으로 해도 됐다. 50이었는데... */
+}
+</style>
--- frontend/src/components/search/ProgramSearch.vue
+++ frontend/src/components/search/ProgramSearch.vue
@@ -29,7 +29,9 @@
       </b-input-group>
     </b-nav>
 
-    <ResultItems :programs="result"/>
+    <ResultProgram v-if="current_option == 1" :programs="result"/>
+    <ResultPeople v-if="current_option == 2" :peoples="result"/>
+
     <!-- v-if를 넣어서 처음 페이지 띄웠을 때 (검색버튼 안 눌렀을 때) 는 동작 안하게 함 -->
     <infinite-loading v-if="loading_complete && !isEndPoint" @infinite="infiniteHandler"></infinite-loading>
 
@@ -42,19 +44,22 @@ import VueBootstrapTypeahead from 'vue-bootstrap-typeahead';
 import SearchApi from '@/api/SearchApi.js';
 import tmdbApi from '@/api/tmdbApi.js';
 import axios from "axios";
-import ResultItems from "@/components/search/ProgramSearchResult.vue";
+import ResultProgram from "@/components/search/ProgramSearchResult.vue";
+import ResultPeople from "@/components/search/PeopleSearchResult.vue";
 import InfiniteLoading from 'vue-infinite-loading';
 
 export default {
 
   data() {
     return {
+      // selectbox 관련 속성
       selected: 'program',
       options: [
         { value: 'program', text: "프로그램명" },
         { value: 'people', text: "출연자"}
       ],
-
+      current_option: 0,
+      
       word: "",
       searchedList: [],
       result: [],
@@ -98,7 +103,7 @@ export default {
         axios.get(tmdbApi.BASE_URL + "search/tv?query=" + this.word + "&api_key=" + tmdbApi.API_KEY + "&page=" + this.current_page + "&language=ko")
             .then(res => {
                 console.log(res);
-                this.programs_result = this.programs_result.concat(res.data.results);
+                this.result = this.result.concat(res.data.results);
                 this.loading_complete = true;
             })
             .catch(error => {
@@ -143,6 +148,7 @@ export default {
                 this.result = res.data.results;
                 this.total_pages = res.data.total_pages;
                 this.current_page = 1;
+                this.current_option = 1;
                 this.loading_complete = true;
             })
             .catch(error => {
@@ -156,19 +162,40 @@ export default {
               // 1. 동명이인 어떻게 처리? - 다 띄워줄 수 있게 인터페이스를...?
               // 2. known_for가 왜 3개까지밖에 안뜨지?
                 console.log(res);
-                
-                let items = res.data.results[0].known_for;
-                // 영화빼고 TV 프로그램만 넣기.
-                for (let i=0; i<items.length; i++){
-                  if (items[i].media_type == 'tv')
-                    this.result.push(items[i]);
+                let promises = [];
+                this.result = [];
+
+                let peoples = res.data.results;
+                // 검색결과로 나온 인물들에 대하여 detail 검색하기.
+                for (let i=0; i<peoples.length; i++){
+                  promises.push(this.detailSearchForPeople(peoples[i].name, peoples[i].profile_path, peoples[i].id));
                 }
                 
-                //this.result = res.data.results[0].known_for; 
-                
-                this.total_pages = res.data.total_pages;
-                this.current_page = 1;
-                this.loading_complete = true;
+                // Promise.all: async랑 같이 써서 위의 작업이 다 끝나고 나서 실행되는 것을 보장하기 위함
+                Promise.all(promises).then( () => {
+                  console.log(this.result);
+                  this.total_pages = res.data.total_pages;
+                  this.current_page = 1;
+                  this.current_option = 2;
+                  this.loading_complete = true;
+                })
+
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
+
+    async detailSearchForPeople(name, pic, id){
+        axios.get(tmdbApi.BASE_URL + "person/" + id + "/tv_credits?api_key=" + tmdbApi.API_KEY + "&language=ko")
+            .then(res => {
+                console.log(res);
+                let item = {};
+                item.id = id;
+                item.people_name = name;
+                item.profile_path = pic;
+                item.programs = res.data.cast;
+                this.result.push(item);
             })
             .catch(error => {
                 console.log(error);
@@ -181,7 +208,8 @@ export default {
   components: {
     //SearchApi,
     VueBootstrapTypeahead,
-    ResultItems,
+    ResultProgram,
+    ResultPeople,
     InfiniteLoading,
   }
 }
