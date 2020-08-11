486e159d760d0da5d068f8664b660cf73d369099
손초능 <thschsmd@naver.com>
2020-08-11 10:27:45 +0900

    [Feat] 프로그램 공유 기능 완성 수정: CreateShare.vue, FeedProgramThumbnail.vue, FeedShareThumbnail.vue

--- frontend/src/components/feed/feedThumbnail/FeedProgramThumbnail.vue
+++ frontend/src/components/feed/feedThumbnail/FeedProgramThumbnail.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
-
+  <div class="row">
+    <img class="col-4" :src="'https://image.tmdb.org/t/p/w500'+program.programDetail.backdrop_path">
+    <div class="col-8">
+      {{program.programDetail.name}}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ProgramThumbnail',
-  
+  props: {
+    program: Object,
+  }
 }
 </script>
 
