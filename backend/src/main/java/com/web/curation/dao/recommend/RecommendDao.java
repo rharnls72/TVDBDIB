8f6ff6eafb9d9a63c20afe5d51fce8dae7664086
Minseong Seo <cbnm943@gmail.com>
2020-08-11 11:32:38 +0900

    [Feat] 추천 시스템을 위한 백엔드 작업중 수정: application.properties, dao/recommend/*, RecommendData.java

---
+++ backend/src/main/java/com/web/curation/dao/recommend/RecommendDao.java
@@ -0,0 +1,10 @@
+package com.web.curation.dao.recommend;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface RecommendDao {
+    
+}
