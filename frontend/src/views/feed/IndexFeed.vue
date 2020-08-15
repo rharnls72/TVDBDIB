4ce13a90b4a85b890e25ee449756134079ca8a4a
손초능 <thschsmd@naver.com>
2020-08-16 04:13:18 +0900

    [Fix] 피드 리스트 노출 수정 수정 : WrittenFeed.vue, CreateShare.vue, FeedItem.vue, CreateHeader.vue, FeedProgramThumbnail.vue, FeedShareEpisodeThumbnail.vue, FeedShareThumbnail.vue, IndexScrap.vue, MyPage.vue, IndexFeed.vue

--- frontend/src/views/feed/IndexFeed.vue
+++ frontend/src/views/feed/IndexFeed.vue
@@ -8,7 +8,7 @@
         <feedVoteItem v-if="d.ctype===3" :article="d" :fno="d.fno" @deleteItem="removeFeed"/> -->
         <FeedItem :article="feed" :fno="feed.fno" @deleteItem="removeFeed"/>
       </div>
-      <div class="myfeed"></div>
+      <hr class="row col-12">
     </div>
     <Footer/>
   </div>
@@ -88,6 +88,10 @@ export default {
 </script>
 
 <style scoped>
+  .last {
+    height: 100px;
+    padding: 100px;
+  }
   .myfeed {
     padding-top: 70px;
     margin-bottom: 10px;
