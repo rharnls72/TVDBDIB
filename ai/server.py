839f8bb537ed6eee02524624c0f11519963e56b6
Minseong Seo <cbnm943@gmail.com>
2020-08-18 10:37:59 +0900

    [Fix] 이제부터 마스터에 직접 작업할래요 수정: Dockerfile, serverpy

--- ai/server.py
+++ ai/server.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 @app.route('/ai/recommend', methods=['GET'])
 def getRecommends():
     uno = str(request.args["uno"])
-    return {'data': ml.model_load(int(uno))}
+    print("Get uno: " + uno)
+    uno = int(uno)
+    print("Int uno: " + str(uno))
+    return {'data': ml.model_load(uno)}
 
 if __name__ == '__main__':
     app.run(port=8888)
