d85c6403b6aa188edc728d54144c0fadb2f55d5b
Minseong Seo <cbnm943@gmail.com>
2020-08-18 11:13:33 +0900

    [Fix] 곧 점심시간이에요

--- ai/model_generate.py
+++ ai/model_generate.py
@@ -4,117 +4,118 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-"""
-## First, load the data and apply preprocessing
-"""
+def model_generate():
+    """
+    ## First, load the data and apply preprocessing
+    """
 
-ratings_file = "/tvility/testData.csv"
-df = pd.read_csv(ratings_file)
+    ratings_file = "/tvility/testData.csv"
+    df = pd.read_csv(ratings_file)
 
-print(df)
+    print(df)
 
-"""
-First, need to perform some preprocessing to encode users and movies as integer indices.
-"""
-user_ids = df["userId"].unique().tolist()
-user2user_encoded = {x: i for i, x in enumerate(user_ids)}
-userencoded2user = {i: x for i, x in enumerate(user_ids)}
-movie_ids = df["programId"].unique().tolist()
-movie2movie_encoded = {x: i for i, x in enumerate(movie_ids)}
-movie_encoded2movie = {i: x for i, x in enumerate(movie_ids)}
-df["user"] = df["userId"].map(user2user_encoded)
-df["movie"] = df["programId"].map(movie2movie_encoded)
+    """
+    First, need to perform some preprocessing to encode users and movies as integer indices.
+    """
+    user_ids = df["userId"].unique().tolist()
+    user2user_encoded = {x: i for i, x in enumerate(user_ids)}
+    userencoded2user = {i: x for i, x in enumerate(user_ids)}
+    movie_ids = df["programId"].unique().tolist()
+    movie2movie_encoded = {x: i for i, x in enumerate(movie_ids)}
+    movie_encoded2movie = {i: x for i, x in enumerate(movie_ids)}
+    df["user"] = df["userId"].map(user2user_encoded)
+    df["movie"] = df["programId"].map(movie2movie_encoded)
 
-num_users = len(user2user_encoded)
-num_movies = len(movie_encoded2movie)
-df["rating"] = df["rating"].values.astype(np.float32)
-# min and max ratings will be used to normalize the ratings later
-min_rating = min(df["rating"])
-max_rating = max(df["rating"])
+    num_users = len(user2user_encoded)
+    num_movies = len(movie_encoded2movie)
+    df["rating"] = df["rating"].values.astype(np.float32)
+    # min and max ratings will be used to normalize the ratings later
+    min_rating = min(df["rating"])
+    max_rating = max(df["rating"])
 
-print(
-    "Number of users: {}, Number of Movies: {}, Min rating: {}, Max rating: {}".format(
-        num_users, num_movies, min_rating, max_rating
+    print(
+        "Number of users: {}, Number of Movies: {}, Min rating: {}, Max rating: {}".format(
+            num_users, num_movies, min_rating, max_rating
+        )
     )
-)
 
-"""
-## Prepare training and validation data
-"""
-df = df.sample(frac=1, random_state=42)
-x = df[["user", "movie"]].values
-# Normalize the targets between 0 and 1. Makes it easy to train.
-y = df["rating"].apply(lambda x: (x - min_rating) / (max_rating - min_rating)).values
-# Assuming training on 90% of the data and validating on 10%.
-train_indices = int(0.9 * df.shape[0])
-x_train, x_val, y_train, y_val = (
-    x[:train_indices],
-    x[train_indices:],
-    y[:train_indices],
-    y[train_indices:],
-)
+    """
+    ## Prepare training and validation data
+    """
+    df = df.sample(frac=1, random_state=42)
+    x = df[["user", "movie"]].values
+    # Normalize the targets between 0 and 1. Makes it easy to train.
+    y = df["rating"].apply(lambda x: (x - min_rating) / (max_rating - min_rating)).values
+    # Assuming training on 90% of the data and validating on 10%.
+    train_indices = int(0.9 * df.shape[0])
+    x_train, x_val, y_train, y_val = (
+        x[:train_indices],
+        x[train_indices:],
+        y[:train_indices],
+        y[train_indices:],
+    )
 
-"""
-## Create the model
-We embed both users and movies in to 50-dimensional vectors.
-The model computes a match score between user and movie embeddings via a dot product,
-and adds a per-movie and per-user bias. The match score is scaled to the `[0, 1]`
-interval via a sigmoid (since our ratings are normalized to this range).
-"""
-EMBEDDING_SIZE = 50
-
-
-class RecommenderNet(keras.Model):
-    def __init__(self, num_users, num_movies, embedding_size, **kwargs):
-        super(RecommenderNet, self).__init__(**kwargs)
-        self.num_users = num_users
-        self.num_movies = num_movies
-        self.embedding_size = embedding_size
-        self.user_embedding = layers.Embedding(
-            num_users,
-            embedding_size,
-            embeddings_initializer="he_normal",
-            embeddings_regularizer=keras.regularizers.l2(1e-6),
-        )
-        self.user_bias = layers.Embedding(num_users, 1)
-        self.movie_embedding = layers.Embedding(
-            num_movies,
-            embedding_size,
-            embeddings_initializer="he_normal",
-            embeddings_regularizer=keras.regularizers.l2(1e-6),
-        )
-        self.movie_bias = layers.Embedding(num_movies, 1)
-
-    def call(self, inputs):
-        user_vector = self.user_embedding(inputs[:, 0])
-        user_bias = self.user_bias(inputs[:, 0])
-        movie_vector = self.movie_embedding(inputs[:, 1])
-        movie_bias = self.movie_bias(inputs[:, 1])
-        dot_user_movie = tf.tensordot(user_vector, movie_vector, 2)
-        # Add all the components (including bias)
-        x = dot_user_movie + user_bias + movie_bias
-        # The sigmoid activation forces the rating to between 0 and 1
-        return tf.nn.sigmoid(x)
+    """
+    ## Create the model
+    We embed both users and movies in to 50-dimensional vectors.
+    The model computes a match score between user and movie embeddings via a dot product,
+    and adds a per-movie and per-user bias. The match score is scaled to the `[0, 1]`
+    interval via a sigmoid (since our ratings are normalized to this range).
+    """
+    EMBEDDING_SIZE = 50
 
 
-model = RecommenderNet(num_users, num_movies, EMBEDDING_SIZE)
-model.compile(
-    loss=tf.keras.losses.BinaryCrossentropy(), optimizer=keras.optimizers.Adam(lr=0.001)
-)
+    class RecommenderNet(keras.Model):
+        def __init__(self, num_users, num_movies, embedding_size, **kwargs):
+            super(RecommenderNet, self).__init__(**kwargs)
+            self.num_users = num_users
+            self.num_movies = num_movies
+            self.embedding_size = embedding_size
+            self.user_embedding = layers.Embedding(
+                num_users,
+                embedding_size,
+                embeddings_initializer="he_normal",
+                embeddings_regularizer=keras.regularizers.l2(1e-6),
+            )
+            self.user_bias = layers.Embedding(num_users, 1)
+            self.movie_embedding = layers.Embedding(
+                num_movies,
+                embedding_size,
+                embeddings_initializer="he_normal",
+                embeddings_regularizer=keras.regularizers.l2(1e-6),
+            )
+            self.movie_bias = layers.Embedding(num_movies, 1)
 
-"""
-## Train the model based on the data split
-"""
-# tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
+        def call(self, inputs):
+            user_vector = self.user_embedding(inputs[:, 0])
+            user_bias = self.user_bias(inputs[:, 0])
+            movie_vector = self.movie_embedding(inputs[:, 1])
+            movie_bias = self.movie_bias(inputs[:, 1])
+            dot_user_movie = tf.tensordot(user_vector, movie_vector, 2)
+            # Add all the components (including bias)
+            x = dot_user_movie + user_bias + movie_bias
+            # The sigmoid activation forces the rating to between 0 and 1
+            return tf.nn.sigmoid(x)
 
-history = model.fit(
-    x=x_train,
-    y=y_train,
-    batch_size=128,
-    epochs=15,
-    verbose=1,
-    validation_data=(x_val, y_val),
-    # callbacks=[tensorboard] # 텐서보드 로그 남기기... 근데 도움은 안됐음
-)
 
-model.save('/tvility/test_model', save_format='tf') # 모델 저장하기. 디렉토리에 test_model이란 폴더로 생김
+    model = RecommenderNet(num_users, num_movies, EMBEDDING_SIZE)
+    model.compile(
+        loss=tf.keras.losses.BinaryCrossentropy(), optimizer=keras.optimizers.Adam(lr=0.001)
+    )
+
+    """
+    ## Train the model based on the data split
+    """
+    # tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
+
+    history = model.fit(
+        x=x_train,
+        y=y_train,
+        batch_size=128,
+        epochs=15,
+        verbose=1,
+        validation_data=(x_val, y_val),
+        # callbacks=[tensorboard] # 텐서보드 로그 남기기... 근데 도움은 안됐음
+    )
+
+    model.save('/tvility/test_model', save_format='tf') # 모델 저장하기. 디렉토리에 test_model이란 폴더로 생김
--- ai/server.py
+++ ai/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import model_load as ml
+import model_generate as mg
 
 app = Flask(__name__)
 
@@ -12,4 +13,5 @@ def getRecommends():
     return {'data': ml.model_load(uno)}
 
 if __name__ == '__main__':
+    mg.model_generate()
     app.run(port=8888)
