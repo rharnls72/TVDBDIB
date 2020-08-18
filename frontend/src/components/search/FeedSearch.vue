b0b39ddba3bc86da49638228f79ecfff48e3ac00
yurim <fnladl72@naver.com>
2020-08-19 00:44:47 +0900

    [Fix] 검색창 통합 (피드)

--- frontend/src/components/search/FeedSearch.vue
+++ frontend/src/components/search/FeedSearch.vue
@@ -1,29 +1,6 @@
 <template>
 <div>
-    <b-nav justified class="myheader" >
-      <b-input-group class="align-items-center m-2 mysearchbar">
-        <div class="input-group-prepend">
-          <div @click="searchIcon()" class="input-group-text py-0" style="border: 0px; background-color: #D8BEFE;">
-            <b-icon-search></b-icon-search>
-          </div>
-        </div>
-
-        <!-- 자동완성 기능 지원하는 검색 창 컴포넌트 -->
-        <vue-bootstrap-typeahead
-          :data="tags"
-          v-model="word"
-          size="sm"
-          textVariant="red"
-          :serializer="t => '#' + t.name"
-          :minMatchingChars='1'
-          :maxMatches='15'
-          placeholder="search feeds by tag name"
-          ref="searchInput"
-          style="height: auto; width: 80%; border: 0px; background-color: #eee;"
-        />
-      </b-input-group>
-    </b-nav>
-
+  <FeedTagSearchResult :tags="tags" :selectTag="selectTag" @update:selectTag="tag => selectTag = tag"/>
     <ResultItems :feeds_result="part_feeds_result"/>
     <infinite-loading v-if="loading_complete && !isEndPoint" @infinite="infiniteHandler"></infinite-loading>
 </div>
@@ -31,10 +8,10 @@
 </template>
 
 <script>
-import VueBootstrapTypeahead from 'vue-bootstrap-typeahead';
 import SearchApi from '@/api/SearchApi.js';
 import ResultItems from "@/components/search/FeedSearchResult.vue";
 import InfiniteLoading from 'vue-infinite-loading';
+import FeedTagSearchResult from '@/components/search/FeedTagSearchResult.vue';
 
 export default {
   name: 'FeedSearch',
@@ -42,46 +19,58 @@ export default {
     return {
       startPoint: 0,
       interval: 5,
-      word: "",
+      alltags: [],
       tags: [],
       total_feeds_result: [],
       part_feeds_result: [],
       loading_complete: false,
-      isEndPoint: false // 끝까지 도달했으면 더이상 무한스크롤 돌아가는거 안 보이게
+      isEndPoint: false, // 끝까지 도달했으면 더이상 무한스크롤 돌아가는거 안 보이게
+      selectTag: '',
     }
   },
-  // props:{
-  //   word: String,
-  // },
-  watch: {
-    word(newWord) {
-        this.getTagList(newWord);
-    },
+  props:{
+    word: String,
   },
-  
-  methods: {
-
-    getTagList(newWord) {
-        SearchApi.getTagList(
-        newWord
-        , res => {
-            console.log(res);
-            this.tags = res.data.data;
+  watch: {
+    word: function (){
+      this.selectTag = '';
+      // this.searchIcon();
+      this.getTagList();
+    },
+    selectTag: function(){
+      if(this.selectTag!=''){
+        this.tags = [];
+        this.searchIcon();
+      }
+    }
+  },
+  mounted() {
+    SearchApi.getTagList(
+        res => {
+            this.alltags = res.data.data;
+            this.getTagList();
         }
         , err => {
             console.log(err);
         }
+    );
+  },
+  methods: {
+    getTagList() {
+      this.tags = this.alltags.filter(
+          (tag) => tag.name.toUpperCase().includes(this.word.toUpperCase())
         );
     },
 
     searchIcon(){
+      let searchWord = this.word;
         // 맨앞에 # 붙여서 검색했을 경우 알아서 빼주기
         if (this.word.startsWith("#"))
-            this.word = this.word.substring(1);
+            searchWord = this.word.substring(1);
 
         SearchApi.searchByTag(
         {
-            word: this.word,
+            word: searchWord,
             requestCount : 0
         }
         , res => {
@@ -128,10 +117,9 @@ export default {
 
   },
   components: {
-    //SearchApi,
-    VueBootstrapTypeahead,
     ResultItems,
-    InfiniteLoading
+    InfiniteLoading,
+    FeedTagSearchResult
   }
 }
 </script>
