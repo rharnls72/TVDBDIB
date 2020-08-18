6952a63f4e74e5199cdb0d82705a5b2c2d187d52
Minseong Seo <cbnm943@gmail.com>
2020-08-18 10:04:09 +0900

    [Fix] 파이썬 서버에서 uno 제대로 받나요 수정: server.py

--- ai/server.py
+++ ai/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/ai/recommend', methods=['GET'])
 def getRecommends():
     uno = request.args["uno"]
-    print('Get uno : ' + int(str(uno)))
+    print('Get uno : ' + str(uno))
     return {'data': "너무 힘들어요"}
 
 if __name__ == '__main__':
